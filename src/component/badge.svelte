4959c0ae7d6fbf002d91e9536428768d0dbdf8e3
Amjad-722 <aliamjad722ee@gmail>
2025-04-22 14:04:37 +0500

    updated

--- src/component/badge.svelte
+++ src/component/badge.svelte
@@ -15,7 +15,7 @@
 	$: sizeClass = {
 		sm: 'px-1 py-1 text-[10px] leading-[14px] font-semibold rounded-[8px]',
 		md: 'px-2 py-2 text-[14px] leading-[14px] font-semibold rounded-[8px]',
-		lg: 'px-2 py-2 text-[16px] leading-[14px] font-medium rounded-[8px]',
+		lg: 'px-2 py-2 text-[16px] leading-[14px] font-semibold rounded-[8px]',
 		full: 'w-full py-2.5 text-lg'
 	}[size];
 
