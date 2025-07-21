ca9eb366db17758216a86436e35aca7972362694
Amjad-722 <aliamjad722ee@gmail.com>
2025-07-21 14:55:13 +0500

    Commit 2: Add hover effect with transition to list items

--- src/component/each-loop.svelte
+++ src/component/each-loop.svelte
@@ -12,7 +12,7 @@ let items = [
    
         <ul class="flex flex-col gap-6 items-center">
         {#each items as item}
-        <li class="text-xl font-semibold rounded-lg px-4 py-2 shadow-sm bg-gray-50" style="color: {item.hex}">{item.name}</li>
+        <li class="text-xl font-semibold rounded-lg px-4 py-2 shadow-sm bg-gray-50 hover:bg-gray-100 transition-colors duration-200" style="color: {item.hex}">{item.name}</li>
         {/each}
         </ul>
 </div>
